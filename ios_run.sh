ccb8ab4620dc3d083b89e8e65db6c68495cc0232
Birh Burh <birhburh@gmail.com>
2024-07-18 13:54:07 +0200

    Update ios_run to work correctly with weird simulator names

--- ios_run.sh
+++ ios_run.sh
@@ -3,10 +3,13 @@
 set -e
 cargo build --target x86_64-apple-ios --release
 cp target/x86_64-apple-ios/release/maq_tic_tac_toe MaqTicTacToe.app
+# MODEL="iPhone 14"
+MODEL="iPad (10th generation)"
 # only once, to get the emulator running
-IPHONE_8_INFO=( $(xcrun simctl list | grep " iPhone 8" | tr -s ' ' | sed 's/[^(]* (\([^(]*\)).*(\(.*\))/\1 \2/') )
-if [ "${IPHONE_8_INFO[1]}" != "Booted" ]; then
-    xcrun simctl boot "${IPHONE_8_INFO[0]}"
+SIMULATOR_INFO=( $(xcrun simctl list | grep " $MODEL (" | awk '{gsub(/[()]/, "", $(NF-1)); gsub(/[()]/, "", $NF); print $(NF-1)" "$NF}') )
+echo "${SIMULATOR_INFO[@]}"
+if [ "${SIMULATOR_INFO[1]}" != "Booted" ]; then
+    xcrun simctl boot "${SIMULATOR_INFO[0]}"
 fi
 xcrun simctl install booted MaqTicTacToe.app
 xcrun simctl launch booted com.maq_tic_tac_toe
